adafe8ff739d19b3f022eee279da9dae0e4dc0ad
Arushi Somani <arushisomani@berkeley.edu>
2023-07-03 00:03:59 -0700

    adding ability to delete notes

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { Input } from "postcss";
-import { getNotes, getUser, createNote } from "../lib/utils";
+import { getNotes, getUser, createNote, deleteNote } from "../lib/utils";
 import { useRouter } from 'next/router';
 import ReactMarkdown from 'react-markdown';
 import Head from "next/head";
@@ -28,10 +28,10 @@ const PageHeader = () => {
 
 const Loader = () => {
   return (
-  <>
-    <PageHeader />
-    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
-  </>
+    <>
+      <PageHeader />
+      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+    </>
   )
 }
 
@@ -62,17 +62,17 @@ const InputBar = ({ user, username, slug, setNotes, setModalIsOpen }: {
           {/* Modal panel, show when screen is small */}
           <div className="inline-block align-bottom bg-gray-300 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
             <input type="text" placeholder="Enter your note here" className="p-4 w-full bg-gray-300  outline-none"
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }} />
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }} />
             {/* Center button */}
             <button className="border-2 border-gray-700 rounded-lg p-1 m-2 w-20 hover:bg-gray-700"
-            onClick={async () => {
-            await createNote("", description, user.email);
-            // Reload the component
-            const res = await getNotes(slug as string);
-            setNotes(res.notes.reverse());
-          }}>+</button>
+              onClick={async () => {
+                await createNote("", description, user.email);
+                // Reload the component
+                const res = await getNotes(slug as string);
+                setNotes(res.notes.reverse());
+              }}>+</button>
           </div>
         </div>
       </div>
@@ -144,31 +144,31 @@ export default function NotesDisplay() {
         <span className="special">{slug}</span>{"'s"} notes
       </h1>
       <div className="flex flex-row justify-center">
-      {
-        session && !user
-          ? (<Loader />)
-          :
-          (
-            user && user?.username && slug === user?.username && <button onClick={() => {
-            setModalIsOpen(true);
-          }}
-            className={`text-sm border-2 border-gray-700 rounded-lg p-2 m-2 w-30 hover:bg-gray-700 hover:text-gray-100 ${font.className}`}
-          > Create New
-          </button>
-          )
-      }
-      {modalIsOpen
-        && <InputBar user={user} username={user?.username} slug={slug} setNotes={setNotes} setModalIsOpen={setModalIsOpen}/>
-      }
+        {
+          session && !user
+            ? (<Loader />)
+            :
+            (
+              user && user?.username && slug === user?.username && <button onClick={() => {
+                setModalIsOpen(true);
+              }}
+                className={`text-sm border-2 border-gray-700 rounded-lg p-2 m-2 w-30 hover:bg-gray-700 hover:text-gray-100 ${font.className}`}
+              > Create New
+              </button>
+            )
+        }
+        {modalIsOpen
+          && <InputBar user={user} username={user?.username} slug={slug} setNotes={setNotes} setModalIsOpen={setModalIsOpen} />
+        }
       </div>
       <div className="pt-8">
-        { notes === null
-        ?<div className="flex flex-col items-center"> <Loader /></div>
+        {notes === null
+          ? <div className="flex flex-col items-center"> <Loader /></div>
 
-        :
+          :
           notes.map((note: any) => {
             return (
-              <NoteDisplay description={note.description} timestamp={note.createdAt} key={note._id} />
+              <NoteDisplay description={note.description} timestamp={note.createdAt} setNotes={setNotes} slug={slug} email={session.user.email} key={note._id} />
             )
           })
         }
@@ -177,8 +177,8 @@ export default function NotesDisplay() {
   )
 }
 
-const NoteDisplay = ({ description, timestamp }: {
-  description: string, timestamp: string
+const NoteDisplay = ({ description, timestamp, setNotes, slug, email }: {
+  description: string, timestamp: string, setNotes: any, slug: any, email: any
 }) => (
   <div
     className={`flex flex-col md:flex-row md:items-center pb-8 ${jbm.className}`}
@@ -197,8 +197,38 @@ const NoteDisplay = ({ description, timestamp }: {
         })
       }
     </p>
+    <div onClick={async () => {
+      await deleteNote(description, email);
+      const res = await getNotes(slug as string);
+      setNotes(res.notes.reverse());
+
+    }} className="pr-2 cursor-pointer">
+      <TrashCan />
+    </div>
     <div className="pl-5 md:pl-0 text-sm md:w-1/2">
       <ReactMarkdown>{description}</ReactMarkdown>
     </div>
   </div>
 );
+
+
+const TrashCan = () => (
+  <svg width="15" height="15"
+    viewBox="0 0 128 128"
+    aria-labelledby="title description"
+    xmlns="http://www.w3.org/2000/svg">
+
+    <title id="title">Trash Icon</title>
+    <desc id="description">This is a trash icon</desc>
+
+    {/* Change to red */}
+    <rect x="32" y="40" width="64" height="76" stroke="#FF0000"
+    stroke-width="6" fill="transparent" rx="1" ry="1" />
+    <polygon points="28,24 100,24 104,40 24,40" stroke="#FF0000" stroke-width="6" fill="transparent" stroke-linejoin="round" />
+    <rect x="56" y="12" width="16" height="12" stroke="#FF0000" stroke-width="6" fill="transparent" rx="1" ry="1" />
+    <line x1="48" y1="92" x2="48" y2="68" stroke="#FF0000" stroke-width="6" stroke-linecap="round" />
+    <line x1="64" y1="92" x2="64" y2="68" stroke="#FF0000" stroke-width="6" stroke-linecap="round" />
+    <line x1="80" y1="92" x2="80" y2="68" stroke="#FF0000" stroke-width="6" stroke-linecap="round" />
+
+  </svg>
+)
